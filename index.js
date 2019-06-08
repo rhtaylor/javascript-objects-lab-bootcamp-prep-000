ea1d4e345c00a656ed74f7aa4c328a59862acd81
Randall Taylor <randyisbusy@gmail.com>
2019-06-08 23:28:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const recipes = { prop: 1};
-function updateObjectWithKeyAndValue(object, key, value){ var newOb = Object.assign(recipes, { [key] : value}); return newOb}
+const recipes = {};
+function updateObjectWithKeyAndValue(object, key, value){ var newOb = Object.assign(recipes, object, { [key] : value}); return newOb}
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){ Object.assign(recipes, { [key] : value}); return recipes}
