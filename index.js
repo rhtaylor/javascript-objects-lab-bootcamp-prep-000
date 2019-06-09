184472d84d609708dd88c6a0cc3e2d63bbcfdd7b
Randall Taylor <randyisbusy@gmail.com>
2019-06-09 01:09:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){ var newOb = Object.assign(recipes, object, { [key] : value}); return newOb}
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){ Object.assign(object, { [key] : value}); return object}
-function deleteFromObjectByKey(object, key){ var newOb = object; delete newOb[key]; return newOb }
+function deleteFromObjectByKey(object, key){ const nunu = Object.assign(object); delete nunu[key]; return nunu }
